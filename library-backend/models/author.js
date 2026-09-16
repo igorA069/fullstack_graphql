2e1716cc5da461a00275b8501157cd50099f8dcc
Igor Achieser <igor.achieser@aumovio.com>
2026-09-16 18:45:55 +0200

    Initial commit (transfer from fullstackopen/part8)

---
+++ library-backend/models/author.js
@@ -0,0 +1,13 @@
+const { mongoose } = require("mongoose");
+
+const authorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 4,
+    unique: true,
+    required: true,
+  },
+  born: Number,
+});
+
+module.exports = mongoose.model("Author", authorSchema);
