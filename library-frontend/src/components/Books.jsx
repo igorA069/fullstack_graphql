cca151e88cb0c971d785ca3f7f4ffca4bed01c0b
Igor Achieser <igor.achieser@aumovio.com>
2026-09-20 18:55:54 +0200

    Ex 5.20: implement filtering by genre

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 import { useQuery } from "@apollo/client/react";
 
 import { ALL_BOOKS } from "../queries";
 
 const Books = (props) => {
+  const [selectedGenres, setSelectedGenres] = useState(new Set());
+
   const result = useQuery(ALL_BOOKS);
 
   if (!props.show || !result.data) {
@@ -11,10 +15,15 @@ const Books = (props) => {
 
   const books = result.data.allBooks;
 
+  // transform an array of books that has an array of genres each into a set of overall occurring genres:
+  const genres = books.reduce((accumulator, currentValue) => {
+    currentValue.genres.forEach((genre) => accumulator.add(genre));
+    return accumulator;
+  }, new Set());
+
   return (
     <div>
       <h2>books</h2>
-
       <table>
         <tbody>
           <tr>
@@ -22,15 +31,43 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.id}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books.map(
+            (book) =>
+              (selectedGenres.size === 0 /* if no filters are set */ ||
+                book.genres.some((genre) =>
+                  selectedGenres.has(genre),
+                )) /* if any of the book's genres are among the filter genres */ && (
+                <tr key={book.id}>
+                  <td>{book.title}</td>
+                  <td>{book.author.name}</td>
+                  <td>{book.published}</td>
+                </tr>
+              ),
+          )}
         </tbody>
       </table>
+
+      <h2>Filter by genre:</h2>
+
+      {Array.from(genres).map((genre) => (
+        <div key={genre}>
+          <label>
+            <input
+              type="checkbox"
+              name={genre}
+              onChange={() => {
+                const updatedSelectedGenres = new Set(selectedGenres);
+                // toggle this genre within the filter:
+                selectedGenres.has(genre)
+                  ? updatedSelectedGenres.delete(genre)
+                  : updatedSelectedGenres.add(genre);
+                setSelectedGenres(updatedSelectedGenres);
+              }}
+            />
+            {genre}
+          </label>
+        </div>
+      ))}
     </div>
   );
 };
