963f3ef9d088be5a765801720fb48b8a3b4c1159
Igor Achieser <igor.achieser@aumovio.com>
2026-09-21 19:15:15 +0200

    Modify allBooksQuery to accept a genre argument

--- library-frontend/src/queries.js
+++ library-frontend/src/queries.js
@@ -14,8 +14,8 @@ export const ALL_AUTHORS = gql`
 `;
 
 export const ALL_BOOKS = gql`
-  query {
-    allBooks {
+  query allBooksQuery($genre: String) {
+    allBooks(genre: $genre) {
       title
       published
       author {
